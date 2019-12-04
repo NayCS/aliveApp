4bcc61be9c0af4a50c16f52e3884646b79937d5a
NayCS <nali.pei.smile@gmail.com>
2019-12-04 11:05:05 -0500

    update server.js dbconnection to sync with heroku

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 //Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactbloodlist");
+const db = require('./models');
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/reactbloodlist';
+mongoose.connect(MONGODB_URI);
+
 
 //Start the API server
 app.listen(PORT, function () {
