980234ef4baee745d3fe9c84d8c1568bdc880ebd
NayCS <nali.pei.smile@gmail.com>
2019-12-03 21:21:57 -0500

    changed up heroku-postbuild

---
+++ client/build/precache-manifest.56900f6a8b5eab9aa26c09c3b6ab97f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8ac4cf930e9ae5d12e9c",
+    "url": "/static/js/main.70c969c7.chunk.js"
+  },
+  {
+    "revision": "f2179e7f22e6577b35cf",
+    "url": "/static/js/2.565b7cae.chunk.js"
+  },
+  {
+    "revision": "8ac4cf930e9ae5d12e9c",
+    "url": "/static/css/main.3ce69565.chunk.css"
+  },
+  {
+    "revision": "496161d1705eb85748a8bde6f9ca1991",
+    "url": "/index.html"
+  }
+];
