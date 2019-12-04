33baead4a9f29ef69638629bf40287d7ef58a2de
NayCS <nali.pei.smile@gmail.com>
2019-12-03 21:05:43 -0500

    modify server.js and package.json for heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const passport = require("passport");
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 
@@ -13,7 +13,12 @@ app.use(express.json());
 //Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
+
 
 //Add routes, both API and view
 app.use(routes);
