1d3eace9955634dea66bb2dcb1b7612df439ef52
NayCS <nali.pei.smile@gmail.com>
2019-12-05 18:15:57 -0500

    moved images

--- client/src/components/Images/index.js
+++ client/src/components/Images/index.js
@@ -1,10 +1,9 @@
 import React from "react";
-import kgiPicture from "./KgiPicture.png";
 
 function ImageKGI() {
     return (
         <React.Fragment>
-            <img src={kgiPicture} width="500" length="500" />
+            <img src="/KgiPicture.png" width="500" length="500" />
 
         </React.Fragment>
     );
--- client/src/components/imagines2/index.js
+++ client/src/components/imagines2/index.js
@@ -1,10 +1,9 @@
 import React from "react";
-import KetoMountain from "./KetoMountain.png";
 
 function ImageMountain() {
     return (
         <React.Fragment>
-            <img src={KetoMountain} width="450" length="450" />
+            <img src="/KetoMountain.png" width="450" length="450" />
 
         </React.Fragment>
     );
