8e3e579d3ba2622428f84a203716df17df9cc2d6
NayCS <nali.pei.smile@gmail.com>
2019-12-05 15:49:29 -0500

    update blood.js

--- client/src/pages/Blood.js
+++ client/src/pages/Blood.js
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom"; import { Col, Row, Container } from "..
 import { List, ListItem } from "../components/List";
 import { Input, FormBtn } from "../components/Form";
 
+
+
 class Blood extends Component {
     state = {
         blood: [],
@@ -59,16 +61,52 @@ class Blood extends Component {
 
     render() {
         return (
-            <Container fluid>
+            <Container fluid style={{ BackgroundColor: "#008080" }} >
                 <Row>
-                    <Col size="md-6">
+                    <Col size="md-7">
                         <Jumbotron>
                             <h1>Track your Glucose Ketone Index</h1>
                         </Jumbotron>
-                        <form>
-                            <label>
-                                Input Fasting Glucose level:
+                        <form >
 
+                            <label>
+                                <strong>
+                                    <h2>What is GKI?</h2>
+                                </strong>
+                            </label>
+
+                            <label>
+
+                                <h4>Glucose Ketone Index (GKI), is a simple formula that tracks the
+                                     ratio of blood glucose to ketones as a single value. But more important,
+                                     it’s a biomarker for tracking your metabolic health as well as your level of ketosis,
+                                          and it’s easily tracked if you have a blood glucose and ketones testing meter.</h4>
+
+                            </label>
+
+
+                            <label>
+                                <strong>
+                                    <h2>Why is GKI important to you?</h2>
+                                </strong>
+                            </label>
+
+                            <label>
+
+                                <h4>Once you know your GKI, you can use it as a tool to help track your ketosis and general metabolic status.
+                                     In the medical industry, it is also becoming an increasingly important measure for therapeutic ketosis
+                                     used to manage chronic health conditions, including certain types of cancer, type 2 diabetes, obesity,
+                                      Alzheimer’s disease, Parkinson’s, chronic inflammatory diseases, epilepsy, insulin resistance, and
+                                      traumatic brain injury!
+
+                                      </h4>
+
+                            </label>
+
+                            <label>
+                                <strong>
+                                    Input Fasting Glucose level:
+                            </strong>
                             </label>
 
                             <Input
@@ -78,8 +116,9 @@ class Blood extends Component {
                                 placeholder="Glucose (Required in (mg/dL))"
                             />
                             <label>
-                                Input Fasting Ketone Level:
-
+                                <strong>
+                                    Input Fasting Ketone level:
+                            </strong>
                             </label>
 
                             <Input
@@ -94,14 +133,30 @@ class Blood extends Component {
                                 onClick={this.handleFormSubmit}
                             >
                                 Get your GKI
-                  </FormBtn>
+                            </FormBtn>
+
+
+
+
+
+
+
                         </form>
+
+
+
                     </Col>
 
-                    <Col size="md-6 sm12">
+                    <Col size="md-5 sm12">
                         <Jumbotron>
                             <h1>Your Blood Metrics</h1>
                         </Jumbotron>
+                        <label>
+                            <strong>
+                                Your Glucose Ketone Index Level:
+                            </strong>
+                        </label>
+
                         {this.state.blood.length ? (
                             <List>
                                 {this.state.blood.map(blood => (
