fe9a5c0ab7fac870801b92439cdcbb7ea4b6595d
NayCS <nali.pei.smile@gmail.com>
2019-12-04 19:22:27 -0500

    added cors to server.js

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const path = require('path');
+let cors = require("cors");
 const PORT = process.env.PORT || 3001;
 
 
 //Define middleware here 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,9 +16,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    //app.get('*', (req, res) => {
+    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    //});
 }
 
 
